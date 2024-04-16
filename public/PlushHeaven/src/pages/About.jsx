df901da6ab221afe46cdc724ae8fb12028285a39
itsHellboHere <visalkr976@gmail.com>
2024-04-16 16:41:39 +0530

    added front-end

---
+++ public/PlushHeaven/src/pages/About.jsx
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+    <>
+    <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+    <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">We Are</h1>
+    <div className="stats bg-fuchsia-700 shadow-inner">
+      <div className="stat">
+      <div className="stat-title text-primary-content text-4xl
+      font-bold tracking-widest">
+        plushHeaven
+    </div>
+      </div>
+    </div>
+    </div>
+    <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto ">
+      At <span className="text-2xl font-serif leading-10 -tracking-tighter text-secondary">PlushHeaven</span>, we believe in the transformative power of furniture. From the cozy corner where you unwind after a long day to the elegant centerpiece that ties the room together, furniture shapes not just your space but also your experiences.
+    </p>
+    <h3 className="mt-6 text-xl font-bold leading-none tracking-tight max-w-2xl mx-auto ">Our Story</h3>
+    <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto ">Founded on the principle of blending comfort with style, <span className="text-2xl font-serif leading-10 -tracking-tighter text-secondary">PlushHeaven</span> embarked on its journey with a singular vision: to redefine the way people experience furniture. With a passion for craftsmanship and an eye for detail, we set out to curate collections that elevate every room they grace.</p>
+    
+    </>
+  )
+}
+export default About
