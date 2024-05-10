d0e93db9f79dc37798fb152753e43d593e2abb91
itsHellboHere <visalkr976@gmail.com>
2024-05-10 23:35:38 +0530

    changes in seller dashboard

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,6 +23,20 @@ const register = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 
+const registerSeller = async (req, res) => {
+    const { email, name, password } = req.body;
+    const emailAlreadyExists = await User.findOne({ email })
+    if (emailAlreadyExists) {
+        throw new CustomError.BadRequestError('Email already exists.')
+    }
+    const role = 'seller';
+    const user = await User.create({ name, email, password, role });
+
+    const tokenUser = createTokenUser(user)
+    attachCookiesToResponse({ res, user: tokenUser });
+
+    res.status(StatusCodes.CREATED).json({ user: tokenUser });
+}
 
 const login = async (req, res) => {
     const { email, password } = req.body;
@@ -61,4 +75,5 @@ module.exports = {
     register,
     login,
     logout,
+    registerSeller,
 }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,13 @@
 
 
-const express =require('express')
-const router=express.Router()
+const express = require('express')
+const router = express.Router()
 
-const {register,login,logout}= require('../controllers/authController')
+const { register, login, logout, registerSeller } = require('../controllers/authController')
 
 router.post('/register', register)
+router.post('/register/seller', registerSeller);
 router.post('/login', login)
-router.get('/logout',logout)
+router.get('/logout', logout)
 
-module.exports=router;
+module.exports = router;
