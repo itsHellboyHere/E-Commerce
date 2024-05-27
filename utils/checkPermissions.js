224a0d8050dd2622980970dba7805f641f9627e1
itsHellboHere <visalkr976@gmail.com>
2024-05-28 01:48:32 +0530

    changes in filter

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,7 +1,7 @@
 const CustomError = require('../errors')
 
 const checkPermissions = (requestUser, resourceUserId) => {
-    if (requestUser.role === 'seller') return;
+    if (requestUser.role === 'admin') return;
     if (requestUser.userId === resourceUserId.toString()) return;
     throw new CustomError.UnauthorizedError(
         'Not authorized to access this route'
